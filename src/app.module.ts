cbbf8d4df45d8f6e15e1eed5be8ec0c1878142e1
daniel.biro <daniel.biro@stud.ubbcluj.ro>
2024-05-04 23:03:01 +0300

    Added mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/sandbox-nestjs-uni-student'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
